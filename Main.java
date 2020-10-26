e5ac29313ced6ed3e1b61b98b1e684c42117fcfb
repl.it user <replituser@example.com>
2020-10-26 14:12:46 +0000

    durch 5 teibare Zahlen

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    for (int i=1; i<200; i++){
+      if (i%5==0){
+        System.out.println("Zahl " +i+" is durch 5 teilbar!");
+      }
+    }
   }
 }
